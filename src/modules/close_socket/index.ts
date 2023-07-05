aa0e80319bb788f13b0f2a95e212714071f800c6
LordOfTheVillage <maksim.kyl285@gmail.com>
2023-07-06 00:41:41 +0300

    fix: add updating room list

--- src/modules/close_socket/index.ts
+++ src/modules/close_socket/index.ts
@@ -1,6 +1,7 @@
 import WebSocket from "ws";
 import { Players } from "../../store/players";
 import { Rooms } from "../../store/rooms";
+import { updateRoom } from "../update_room";
 export const closeSocket = (ws: WebSocket) => {
   const player = Players.getPlayerByWs(ws);
   if (player) {
@@ -16,4 +17,5 @@ export const closeSocket = (ws: WebSocket) => {
       }
     }
   }
+  updateRoom(0)
 }
